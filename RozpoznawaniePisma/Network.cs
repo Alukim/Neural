9759dc15c3e99371a00e3f228cdd31030bdf0a2e
Bartosz Kowalski <bartosz.kowalski@ac-project.net>
2017-07-19 21:49:30 +0200

    End implement of neural

--- RozpoznawaniePisma/Network.cs
+++ RozpoznawaniePisma/Network.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RozpoznawaniePisma
 {
@@ -38,6 +34,7 @@ namespace RozpoznawaniePisma
 
         public Network(int inputSize, int outputSize)
         {
+            //Wpisujemy domyślne wartości
             LEARNING_RATE = 0.2;
             currentIteration = 0;
             maximumIteration = 10;
@@ -62,10 +59,102 @@ namespace RozpoznawaniePisma
             }
         }
 
+        private double Activation(double total)
+         => 1.0 / (1.0 + Math.Exp(-total));
 
-        public void TrainNetwork(double[][] inputs, double[][] outputs)
+        private void CalculateOutput(double[] pattern, string output)
         {
+            // Przpisujemy wejścia do warstwy wejściowej sieci
+            for (int i = 0; i < pattern.Length; ++i)
+                InputLayers[i].Value = pattern[i];
 
+            // Obliczamy wejście, wyjście, wartość oczekiwaną oraz błąd pierwszej warstwy
+            for(int i = 0; i < OutputNumber; ++i)
+            {
+                double total = 0.0;
+
+                for (int j = 0; j < InputNumber; ++j)
+                    total += InputLayers[j].Value * InputLayers[j].Weights[i];
+
+                OutputLayers[i].InputSum = total;
+                OutputLayers[i].Output = Activation(total);
+                OutputLayers[i].Target = OutputLayers[i].Value.CompareTo(output) == 0 ? 1.0 : 0.0;
+                OutputLayers[i].Error = (OutputLayers[i].Target - OutputLayers[i].Output) * OutputLayers[i].Output * (1 - OutputLayers[i].Output);
+            }
+        }
+
+        private void BackPropagation()
+        {
+            for(int j = 0; j < OutputNumber; ++j)
+            {
+                for(int i = 0; i < InputNumber; ++i)
+                {
+                    InputLayers[i].Weights[j] += LEARNING_RATE * (OutputLayers[j].Error) * InputLayers[i].Value;
+                }
+            }
+        }
+
+        private double GetError()
+        {
+            double total = 0.0;
+
+            for(int i = 0; i < OutputNumber; ++i)
+            {
+                total += Math.Pow((OutputLayers[i].Target - OutputLayers[i].Output), 2.0) / 2.0;
+            }
+
+            return total;
+        }
+
+        public bool TrainNetwork(double[][] inputs, double[][] outputs)
+        {
+            double currentError = 0.0;
+            double maximumError = 0.0;
+
+            currentIteration = 0;
+            Errors = new double[maximumIteration];
+
+            do
+            {
+                currentError = 0;
+
+                for (int i = 0; i < inputs.Length; ++i)
+                {
+                    CalculateOutput(inputs[i], OutputLayers[i].Value);
+                    BackPropagation();
+
+                    currentError += GetError();
+                }
+
+                Errors[currentIteration] = currentError;
+                ++currentIteration;
+            }
+            while (currentError > maximumError && currentIteration < maximumIteration);
+
+            // Jeżeli maksymalny błąd został osiągnięty w mniejszej liczbie iteracji, to nauka sieci zakończyła się pomyślnie.
+            if (currentIteration <= maximumIteration)
+                return true;
+
+            return false;
+        }
+
+        public void Recognize(double[] input)
+        {
+            // Przpisujemy wejścia do warstwy wejściowej sieci
+            for (int i = 0; i < InputNumber; ++i)
+                InputLayers[i].Value = input[i];
+
+            // Obliczamy wejście, wyjście, wartość oczekiwaną oraz błąd pierwszej warstwy
+            for (int i = 0; i < OutputNumber; ++i)
+            {
+                double total = 0.0;
+
+                for (int j = 0; j < InputNumber; ++j)
+                    total += InputLayers[j].Value * InputLayers[j].Weights[i];
+
+                OutputLayers[i].InputSum = total;
+                OutputLayers[i].Output = Activation(total);
+            }
         }
     }
 }
